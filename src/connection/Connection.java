cefe3a1e03b28ca2f6fcee2210f702f715df360e
chels <chels@Pedro>
2023-12-13 18:54:14 +0100

    Añadir codigo de conexion para base de datos

--- src/connection/Connection.java
+++ src/connection/Connection.java
@@ -8,6 +8,36 @@ package connection;
  *
  * @author Administrador
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class Connection {
+	private Connection conexion;
+	   //Contructor de la clase	
+		public DBConnection() {
+			try {
+				//cargar el driver JDBC
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				
+				//preparar la URL de conexión
+				String url = "jdbc:mysql://localhost/bdTrabajadores";
+				
+				//conectar con la BBDD  --> ponemos la url, el usuario y la contraseña
+				conexion = DriverManager.getConnection(url, "root", "");
+			} catch (ClassNotFoundException e) {
+				System.out.println("Error en la carga del Driver");
+				e.printStackTrace();
+			} catch (SQLException e) {
+				System.out.println("Error conectando a la BBDD");
+				e.printStackTrace();
+			}
+		}
+
+		public Connection getConexion() {
+			return conexion;
+		}
+		
+		
+	}
     
 }
