9316982a1a31a158611ccecd73d870171f455e15
MIGUEL ANGEL DEL MORAL MARTINEZ <miguelangeldelmoral@hotmail.com>
2023-12-18 19:22:46 +0100

    Creado una nueva clase con el dialogNuevoTrabajador

--- src/vistas/VistaInicial.java
+++ src/vistas/VistaInicial.java
@@ -26,30 +26,17 @@ public class VistaInicial extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        dialogNuevoTrabajador = new javax.swing.JDialog();
-
-        javax.swing.GroupLayout dialogNuevoTrabajadorLayout = new javax.swing.GroupLayout(dialogNuevoTrabajador.getContentPane());
-        dialogNuevoTrabajador.getContentPane().setLayout(dialogNuevoTrabajadorLayout);
-        dialogNuevoTrabajadorLayout.setHorizontalGroup(
-            dialogNuevoTrabajadorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        dialogNuevoTrabajadorLayout.setVerticalGroup(
-            dialogNuevoTrabajadorLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 322, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 307, Short.MAX_VALUE)
         );
 
         pack();
@@ -57,6 +44,5 @@ public class VistaInicial extends javax.swing.JFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JDialog dialogNuevoTrabajador;
     // End of variables declaration//GEN-END:variables
 }
