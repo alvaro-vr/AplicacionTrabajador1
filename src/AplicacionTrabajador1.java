c2359b790812e10691cc5fb7d9f8c7678c1e2d0e
Alvaro Villares <alvarovillaresrodriguez@gmail.com>
2023-12-19 16:46:39 +0100

    Creada la vista inicial y el rellenado de la tabla por defecto

--- src/AplicacionTrabajador1.java
+++ src/AplicacionTrabajador1.java
@@ -5,6 +5,7 @@ public class AplicacionTrabajador1 {
     public static void main(String[] args) {
         // TODO code application logic here
         new VistaInicial().setVisible(true);
+        
     }
     
 }
--- src/vistas/VistaInicial.java
+++ src/vistas/VistaInicial.java
@@ -4,6 +4,13 @@
  */
 package vistas;
 
+import dao.DAOTrabajador;
+import dao.*;
+import entidades.Trabajador;
+import java.awt.List;
+import java.util.ArrayList;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Administrador
@@ -13,10 +20,46 @@ public class VistaInicial extends javax.swing.JFrame {
     /**
      * Creates new form VistaInicial
      */
+    
+    private final DAOTrabajador dao = new DAOTrabajadoresImpl();
+    private final String[] cabeceraTabla = {
+        "DNI", "Nombre", "Apedillos", "Sueldo", "Fecha", "Matricula"
+    };
+    private DefaultTableModel modelo = new DefaultTableModel();
+    
     public VistaInicial() {
         initComponents();
+        setLocationRelativeTo(null);
+        prepararTabla();
+        rellenarTabla();
     }
-
+    
+    public void prepararTabla(){
+        modelo = new DefaultTableModel();
+        modelo.addColumn("DNI");
+        modelo.addColumn("NOMBRE");
+        modelo.addColumn("APELLIDOS");
+        modelo.addColumn("SUELDO");
+        modelo.addColumn("FECHA");
+        modelo.addColumn("MATRICULA");
+    }
+    
+    public void rellenarTabla(){
+        ArrayList<Trabajador> trabajadores = dao.getAll();
+        trabajadores.stream().forEach(trabajador -> {
+            ArrayList fila = new ArrayList();
+            fila.add(trabajador.getDni());
+            fila.add(trabajador.getNombre());
+            fila.add(trabajador.getApellidos());
+            fila.add(trabajador.getSueldo());
+            fila.add(trabajador.getFecha());
+            fila.add(trabajador.getMatricula());
+            modelo.addRow(fila.toArray());
+        });
+        
+        tableTrabajadores.setModel(modelo);
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -26,23 +69,105 @@ public class VistaInicial extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        dialogGeneral = new javax.swing.JDialog();
+        scrollPanelTabla = new javax.swing.JScrollPane();
+        tableTrabajadores = new javax.swing.JTable();
+        panelContenedorBotones = new javax.swing.JPanel();
+        btnEliminarTrabajador = new javax.swing.JButton();
+        btnAñadirTrabajador = new javax.swing.JButton();
+        btnModificarTrabajador = new javax.swing.JToggleButton();
+
+        javax.swing.GroupLayout dialogGeneralLayout = new javax.swing.GroupLayout(dialogGeneral.getContentPane());
+        dialogGeneral.getContentPane().setLayout(dialogGeneralLayout);
+        dialogGeneralLayout.setHorizontalGroup(
+            dialogGeneralLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        dialogGeneralLayout.setVerticalGroup(
+            dialogGeneralLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        tableTrabajadores.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {}
+            },
+            new String [] {
+
+            }
+        ));
+        scrollPanelTabla.setViewportView(tableTrabajadores);
+
+        panelContenedorBotones.setBorder(javax.swing.BorderFactory.createTitledBorder("Acciones"));
+
+        btnEliminarTrabajador.setText("Eliminar Trabajador");
+        btnEliminarTrabajador.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEliminarTrabajadorActionPerformed(evt);
+            }
+        });
+        panelContenedorBotones.add(btnEliminarTrabajador);
+
+        btnAñadirTrabajador.setText("Añadir Trabajador");
+        btnAñadirTrabajador.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAñadirTrabajadorActionPerformed(evt);
+            }
+        });
+        panelContenedorBotones.add(btnAñadirTrabajador);
+
+        btnModificarTrabajador.setText("Modificar Trabajador");
+        btnModificarTrabajador.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnModificarTrabajadorActionPerformed(evt);
+            }
+        });
+        panelContenedorBotones.add(btnModificarTrabajador);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 322, Short.MAX_VALUE)
+            .addComponent(scrollPanelTabla, javax.swing.GroupLayout.DEFAULT_SIZE, 729, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(panelContenedorBotones, javax.swing.GroupLayout.DEFAULT_SIZE, 717, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 307, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(scrollPanelTabla, javax.swing.GroupLayout.DEFAULT_SIZE, 340, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(panelContenedorBotones, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnEliminarTrabajadorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEliminarTrabajadorActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnEliminarTrabajadorActionPerformed
+
+    private void btnAñadirTrabajadorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAñadirTrabajadorActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnAñadirTrabajadorActionPerformed
+
+    private void btnModificarTrabajadorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnModificarTrabajadorActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnModificarTrabajadorActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAñadirTrabajador;
+    private javax.swing.JButton btnEliminarTrabajador;
+    private javax.swing.JToggleButton btnModificarTrabajador;
+    private javax.swing.JDialog dialogGeneral;
+    private javax.swing.JPanel panelContenedorBotones;
+    private javax.swing.JScrollPane scrollPanelTabla;
+    private javax.swing.JTable tableTrabajadores;
     // End of variables declaration//GEN-END:variables
 }
